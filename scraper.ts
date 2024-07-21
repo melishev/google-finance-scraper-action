9983b06875db49c68f88cddc2c73288fe981b6ef
Matvey Melishev <melishevm@gmail.com>
2024-07-21 18:04:07 +0200

    fix: fix bug when save scraped data

--- scraper.ts
+++ scraper.ts
@@ -1,55 +1,23 @@
 import fs from 'fs'
 import currenciesRaw from './src/currencies.json'
 
-const now = new Date();
-
 type AllCurrencies = { [x: string]: number }
+
+const now = new Date();
+const primaryCurrency = 'EUR'
 
 const regex = /data-last-price="([^"]*)"/
 const endpoint = (code: string, primaryCode: string) =>
   `https://www.google.com/finance/quote/${code}-${primaryCode}`
 
+// const currencies = ['USD', 'EUR', 'RUB', 'GEL', 'TRY', 'BAM'] as const
+const currencies = currenciesRaw.map((currency) => currency.code)
+const requests = currencies.map((code) => fetch(endpoint(code, primaryCurrency)))
+
 const log = (currencyPair: string, ...message: any[]) => console.log(currencyPair, ...message)
 
-function prepareRequests(primaryCurrency: string) {
-  // const currencies = ['USD', 'EUR', 'RUB', 'GEL', 'TRY', 'BAM'] as const
-  const currencies = currenciesRaw.map((currency) => currency.code)
-  const requests = currencies.map((code) => fetch(endpoint(code, primaryCurrency)))
-
-  return {
-    requests,
-    currencies,
-  }
-}
-
-// async function makeRequest(primaryCurrency: string): Promise<AllCurrencies> {
-//   const { requests, currencies } = prepareRequests(primaryCurrency)
-
-//   // const value = new Map<string, number>();
-//   let collection: AllCurrencies = {}
-
-//   for (const currency of currencies) {
-//     const response = await fetch(endpoint(currency, primaryCurrency), {
-//       method: 'GET',
-//     })
-//     const text = await response.text()
-//     const matches = text.match(regex)
-
-//     if (matches) {
-//       const value = Number(matches[1])
-//       // value.set(currency, Number(matches[1]))
-//       collection = { ...collection, [currency]: value }
-//       log(currency, 'data-last-price:', value)
-//     } else {
-//       log(currency, 'data-last-price not found')
-//     }
-//   }
-
-//   return collection
-// }
-
 async function processResponse(currency: string, response: Response): Promise<number> {
-  let value = NaN
+  let value: number = NaN
 
   const text = await response.text()
   const matches = text.match(regex)
@@ -64,18 +32,19 @@ async function processResponse(currency: string, response: Response): Promise<nu
   return value
 }
 
+async function processResponses(responses: Response[]): Promise<AllCurrencies> {
+  let collection = {}
+
+  for await (const idx of responses.keys()) {
+    const value = await processResponse(currencies[idx], responses[idx])
+    collection = { ...collection, [currencies[idx]]: value }
+  }
+
+  return collection
+}
+
 async function makeRequestSync(primaryCurrency: string) {
-  const { requests, currencies } = prepareRequests(primaryCurrency)
-
-  const value = await Promise.all(requests).then(async (responses) => {
-    return currencies.reduce(async (acc, currency, idx) => {
-      const value = await processResponse(currency, responses[idx])
-      acc = { ...acc, [currency]: value }
-      return acc
-    }, {})
-  })
-
-  return value
+  return await Promise.all(requests).then(async (responses) => await processResponses(responses))
 }
 
 /** The method saves the received data to a file */
@@ -99,5 +68,4 @@ function saveToFile(primaryCurrency: string, data: AllCurrencies) {
   fs.writeFileSync(`${path}/${formattedDate}.json`, JSON.stringify(content, null, 2), 'utf-8')
 }
 
-const primaryCurrency = 'EUR'
 saveToFile(primaryCurrency, await makeRequestSync(primaryCurrency))
